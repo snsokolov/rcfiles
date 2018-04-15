0cd674f1430eb98835d729a9638c29ac781ab6a6
anonymous <snsokolov@users.noreply.github.com>
2018-04-15 13:37:56 -0700

    minor bug fix

--- setup.sh
+++ setup.sh
@@ -4,12 +4,12 @@ mkdir -p github
 # Vimrc
 rm -rf github/vimrc_nedit
 git clone https://github.com/snsokolov/vimrc_nedit github/vimrc_nedit
-ln -fs github/vimrc_nedit/_vimrc $HOME/.vimrc.common
+ln -fs `pwd`/github/vimrc_nedit/_vimrc $HOME/.vimrc.common
 
 # RC files
 rm -rf github/rcfiles
 git clone https://github.com/snsokolov/rcfiles github/rcfiles
-ln -fs github/rcfiles/.gitconfig $HOME/.gitconfig
-ln -fs github/rcfiles/.bashrc.common $HOME/.bashrc.common
-ln -fs github/rcfiles/.cshrc.common $HOME/.cshrc.common
-ln -fs github/rcfiles/.vimrc.work $HOME/.vimrc.work
+ln -fs `pwd`/github/rcfiles/.gitconfig $HOME/.gitconfig
+ln -fs `pwd`/github/rcfiles/.bashrc.common $HOME/.bashrc.common
+ln -fs `pwd`/github/rcfiles/.cshrc.common $HOME/.cshrc.common
+ln -fs `pwd`/github/rcfiles/.vimrc.work $HOME/.vimrc.work
